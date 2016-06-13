9fd4b156eae02f42f55b404d9d53d302aaa9e934
Pete Haddrell <petejhaddrell@gmail.com>
2016-06-13 22:21:09 +0100

    improved handling of removing and building of the grid

--- js/pad.js
+++ js/pad.js
@@ -1,16 +1,40 @@
+var gridSize = 16;
+
 $(document).ready (function()
 {
-	for (var i = 0; i < 16; i++)
+	buildgrid(gridSize);
+});
+
+function cleargrid()
+{
+	var userSize = prompt ("Pleae enter grid size")
+	$('.block').removeClass('highlight');
+	removegrid();
+	buildgrid(userSize);
+}	
+
+function removegrid()
+{
+	$("#container").empty();
+}
+
+function buildgrid (size)
+{
+	for (var x = 0; x < size; x++)
 	{
-		for (var x = 0; x < 16; x++)
+		for (var y = 0; y < size; y++)
 		{
 			var block = $('<div class="block"</div>');
 			block.appendTo('#container');
 		}
 	}
 
-	$(".block").on ("mouseenter", function()
+	//need to put in size altering code for the container currently 16 * 14 = gridsize * (gridsize - 2)
+
+	$('.block').on ("mouseenter", function()
 	{
 		$(this).addClass('highlight');
 	});
-});
+
+	gridSize = size;
+}
