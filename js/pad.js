fc84190b2d9e6dde40e64861af4a585ae3ffef33
Pete Haddrell <petejhaddrell@gmail.com>
2016-06-15 16:29:53 +0100

    made the size of the grid more dynamic

--- js/pad.js
+++ js/pad.js
@@ -30,6 +30,9 @@ function buildgrid (size)
 	}
 
 	//need to put in size altering code for the container currently 16 * 14 = gridsize * (gridsize - 2)
+	var contSize = 14 * size;
+	alert (contSize);
+	$('#container').css ({'width' : contSize, 'height' : contSize});
 
 	$('.block').on ("mouseenter", function()
 	{
